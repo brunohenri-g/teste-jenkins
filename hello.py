390fc83b8de791e47c5056d50003630c4ccd5ae6
Bruno Henri Garcia <brunohenri.g@live.com>
2024-08-10 18:14:31 -0300

    afsdf

--- hello.py
+++ hello.py
@@ -1,7 +1,4 @@
 import os
 
-# Suponha que a variável de ambiente que você quer imprimir seja 'HOME'
-variable_name = 'TESTE'
-variable_value = os.getenv(variable_name)
-
-print(f"Valor da variável de ambiente {variable_name} => {variable_value}")
+for key, value in os.environ.items():
+    print(f"{key}={value}")
